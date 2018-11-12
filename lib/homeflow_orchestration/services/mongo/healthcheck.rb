15d9d75322aa7568a1c76a0363d85309db61ddf7
Bob Farrell <robertanthonyfarrell@gmail.com>
2018-11-12 08:01:51 +0000

    Add skeleton for mongo
    Define interface for Services with shared examples.

---
+++ lib/homeflow_orchestration/services/mongo/healthcheck.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Orchestration
+  module Services
+    module Mongo
+      class Healthcheck
+        def self.start; end
+      end
+    end
+  end
+end
---
+++ spec/homeflow_orchestration/services/mongo_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Orchestration::Services::Mongo do
+  it_behaves_like 'a service', described_class
+end
